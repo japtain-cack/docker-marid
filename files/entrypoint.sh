e8dbf2e38bceb963531be1f48e3043dc21c17800
Nathan Snow <nathan.snow@mimir-tech.org>
2019-08-20 10:56:09 -0600

    updating entrypoint.sh

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export MARID_HOME=/var/opsgenie
+export MARID_HOME=/var/opsgenie/marid
+export MARID_CONF=/etc/opsgenie/marid
+export MARID_LIB=/var/lib/opsgenie/marid
 export REMCO_HOME=/etc/remco
 export REMCO_RESOURCE_DIR=${REMCO_HOME}/resources.d
 export REMCO_TEMPLATE_DIR=${REMCO_HOME}/templates
 
-export CONF_PATH=${CONF_PATH:-'/etc/opsgenie/marid/marid.conf'}
-export LOG_CONF_PATH=${LOG_CONF_PATH:-'/etc/opsgenie/marid/log.properties'}
-export SCRIPTS_DIR=${SCRIPTS_DIR:-'/var/opsgenie/marid/scripts'}
-export LOGS_DIR=${LOGS_DIR:-'/var/opsgenie/marid/log'}
 export MEM_LIMIT=${MEM_LIMIT:-'512'}
 export DJAVAX_NET_DEBUG=${DJAVAX_NET_DEBUG:-''}
+export CLASSPATH=$CLASSPATH:"$MARID_HOME/lib/*"
+export CLASSPATH=$CLASSPATH:"$MARID_LIB/*"
 
 if [[ "${HTTP_SERVER_HOST}" == "eth0" ]]; then
   export HTTP_SERVER_HOST=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
@@ -27,12 +27,12 @@ return sdf.format(date)
 EOF
 
 java \
-  -Dmarid.config=/etc/opsgenie/marid \
-  -Dmarid.conf.path="$CONF_PATH" \
-  -Dmarid.log.conf.path="$LOG_CONF_PATH" \
-  -Dmarid.scripts.dir="$SCRIPTS_DIR" \
-  -Dmarid.logs.dir="$LOGS_DIR" \
-  -Djava.io.tmpdir=/tmp/marid \
+  -Dmarid.config="$MARID_CONF" \
+  -Dmarid.conf.path="$MARID_CONF/marid.conf" \
+  -Dmarid.log.conf.path="$MARID_CONF/log.properties" \
+  -Dmarid.scripts.dir="$MARID_HOME/scripts" \
+  -Dmarid.logs.dir="$MARID_HOME/logs" \
+  -Djava.io.tmpdir="/tmp/marid" \
   -Xms256m \
   -Xmx${MEM_LIMIT}m \
   -XX:MaxPermSize=128m \
@@ -40,5 +40,5 @@ java \
   -server \
   -Djavax.net.debug="$DJAVAX_NET_DEBUG" \
   -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=17777 \
-  -cp MARID_CLASSPATH:/var/lib/opsgenie/marid/* com.ifountain.opsgenie.client.marid.Bootstrap
+  -cp $CLASSPATH com.ifountain.opsgenie.client.marid.Bootstrap
 
